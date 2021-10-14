0290245d83027ab535de757024a1e624a22ad778
linfeng810 <li.linfeng.xjtu@gmail.com>
2021-10-14 09:57:09 +0100

    change tab indent to 2 spaces

--- gmsh_input.F90
+++ gmsh_input.F90
@@ -1,201 +1,201 @@
 ! read in gmsh file into our element types
 
 module gmsh_input
-    use mesh_type
+  use mesh_type
 
-    implicit none
+  implicit none
 
-    public :: gmsh_read 
+  public :: gmsh_read 
+  
+  contains 
+  
+  subroutine gmsh_read(file_name, meshele, meshface, meshvertex)
+    character (len=*), intent(in)::file_name
+    type(element), allocatable, intent(inout)::meshele(:)
+    type(face), allocatable, intent(inout)::meshface(:)
+    type(face), allocatable::bfaces(:) ! this is to store faces on boundaries
+      ! actual mesh faces will have interior faces as well.
+    type(vertex), allocatable, intent(inout)::meshvertex(:)
+    type(vertex), allocatable::vertices(:)  ! this only stores one copy 
+      ! of coordinates at same position. (but there can be multiple nodes)
+    integer :: ierr , i, nnod, idx, ele, ele2, nface, inod
+    integer :: edge_nodes(2)
+    integer :: nelements=0, npoints=0, nbcface=0, nele=0
+    real, dimension(3) :: coor
+    integer, dimension(8):: element_info
+    character(len=255) :: text, t, word
     
-    contains 
+    open(unit=10, file=file_name, status='old', action='read') 
+    do  
+      ! this find string snippet is from:
+      ! https://stackoverflow.com/questions/29125581/fortran-find-string-in-txt-file
+      ! by Fortranner
+      read (10, "(a)", iostat=ierr) text
+      if (ierr /= 0) exit 
+      read (text,*) word 
+      if (word == '$Nodes') then ! start reading nodes information
+        read (10, *) nnod
+        ! print*, 'nnod=', nnod
+        allocate(vertices(nnod))
+        do 
+          read (10, *, iostat=ierr) idx, coor(1), coor(2), coor(3)
+          if (ierr /= 0) exit 
+          vertices(idx)%coor(1) = coor(1)
+          vertices(idx)%coor(2) = coor(2)
+          ! print*, 'coor', coor
+        enddo
+      endif
+      if (word == '$Elements') then ! start reading element information 
+        read (10, *) nelements
+        ! print *, 'nelements=', nelements
+        do  
+          read (10, "(A)", iostat=ierr) text
+          ! print *, text 
+          if (ierr /= 0) exit 
+          if (text(1:12)=="$EndElements") exit
+          i=0
+          ! print *, text
+          do while (text /= "")
+            i=i+1
+            call GetTag(t,text)
+            read(t,*) element_info(i)
+          enddo
+          select case (i)
+          case (6) ! 0D elements (points)
+            npoints = npoints + 1 
+          case (7) ! 1D elements (edges)
+            nbcface = nbcface + 1
+          case (8) ! 2D elements (faces)
+            nele = nele + 1
+          case default 
+            print *, 'something is wrong when parsing Elements data, what is being parsed is:'
+            print *, text
+          end select 
+          ! print*, 'npoints=',npoints, 'nbcface=', nbcface, 'nele=', nele
+        enddo 
+      endif
+    enddo
+    allocate(bfaces(nbcface), meshele(nele)) 
+    rewind (10) ! go back and read again to store node info associated with 
+          ! points, edges, and faces.
+    do  
+      read (10, "(a)", iostat=ierr) text
+      if (ierr /= 0) exit 
+      read (text,*) word 
+      if (word == '$Elements') then ! start reading element information
+        ! jump 0D elements
+        do i=1,npoints+1
+          read(10,*)
+        enddo
+        ! 1D elements
+        do nface=1,nbcface 
+          read (10, "(A)", iostat=ierr) text 
+          i=0
+          do while (text /= "")
+            i=i+1
+            call GetTag(t,text)
+            read(t,*) element_info(i)
+          enddo
+          bfaces(nface)%vertex(1) = element_info(6)
+          bfaces(nface)%vertex(2) = element_info(7)
+          bfaces(nface)%bctype = 1
+          ! print *, 'bfaces=', bfaces(nface)%vertex
+        enddo
+        ! 2D elements
+        do ele=1,nele
+          read (10, "(A)", iostat=ierr) text 
+          i=0
+          do while (text /= "")
+            i=i+1
+            call GetTag(t,text)
+            read(t,*) element_info(i)
+            ! print *, 'text=',text,'t=', t, 'element_info', element_info(i)
+          enddo
+          meshele(ele)%node(1) = element_info(6)
+          meshele(ele)%node(2) = element_info(7)
+          meshele(ele)%node(3) = element_info(8)
+          ! print *, 'meshele=', meshele(ele)
+        enddo
+      endif
+    enddo
+    close(10) ! gmsh file no longer has useful info for us.
+
+    ! now we are going to build edges and ... vertices actually. (DG)
+    allocate(meshvertex(nele*3), meshface(nele*3)) ! every 2D element 
+      ! has 3 vertices and 3 faces
+    do ele = 1,nele 
+      ! brutely build meshvertex and mesh face
+      do inod = 1,3
+        ! global nodes
+        meshvertex( (ele-1)*3+inod )%coor(:) = &
+          vertices( meshele(ele)%node(inod) )%coor(:)
+          ! this gives us a natural local to global
+          ! map. I guess IC-FERST uses the same way?
+        ! print*, 'coordinate for global node', (ele-1)*3+inod, 'is' ,meshvertex( (ele-1)*3+inod )%coor
+        
+        ! global edges' nodes 
+          ! (local numbering)
+          !  3
+          !  | \
+          !  |  \
+          !  3   2
+          !  |    \
+          !  1--1--2
+        if (inod.eq.3) edge_nodes=(/3,1/)
+        if (inod.ne.3) edge_nodes=(/inod, inod+1/)
+        meshface( (ele-1)*3+inod )%vertex(1) = (ele-1)*3+edge_nodes(1)
+        meshface( (ele-1)*3+inod )%vertex(2) = (ele-1)*3+edge_nodes(2)
+        meshele( ele )%face(inod) = (ele-1)*3+inod
+        ! print*, 'global nodes for edge', (ele-1)*3+inod, 'is ', meshface( (ele-1)*3+inod )%vertex
+
+        ! is this meshface a boundary face or an interior face?
+        meshface( (ele-1)*3+inod )%bctype=0 ! default: interior face
+        do i = 1,nbcface
+          ! print *, 'elenode1=', meshele(ele)%node(inod), &
+          !   'elenode2=', meshele(ele)%node(edge_nodes(2)), &
+          !   'bfaces nodes=', bfaces(i)%vertex
+          if ( any( meshele(ele)%node(inod) .eq. bfaces(i)%vertex ) .and. &
+            any( meshele(ele)%node(edge_nodes(2)) .eq. bfaces(i)%vertex)) then
+            meshface( (ele-1)*3+inod )%bctype=1 ! is boundary, assuming Dirichlet
+            exit
+          endif
+        enddo
+        ! print *, 'edge type (bc or not)', meshface( (ele-1)*3+inod )%bctype
+
+        ! now we find and store neighbor elements to this meshface
+        meshface( (ele-1)*3+inod )%neighbor(1) = ele ! first, this face belongs to 
+          ! its own element. Then we need to find its other neighbor
+        if ( meshface( (ele-1)*3+inod )%bctype.eq.0) then
+          do ele2 = 1,nele
+            ! print *, 'elenode1=', meshele(ele)%node(inod), &
+            !   'elenode2=', meshele(ele)%node(edge_nodes(2)), &
+            !   'ele2 nodes = ', meshele(ele2)%node
+            if (any(meshele(ele)%node(inod) .eq. meshele(ele2)%node) .and. &
+              any(meshele(ele)%node(edge_nodes(2)) .eq. meshele(ele2)%node) .and. & 
+              ele .ne. ele2) then
+              meshface( (ele-1)*3+inod )%neighbor(2) = ele2
+              exit
+            endif
+          enddo
+        endif
+        ! print *, 'edge neighbor elements are', meshface( (ele-1)*3+inod )%neighbor
+
+      enddo
+    enddo
     
-    subroutine gmsh_read(file_name, meshele, meshface, meshvertex)
-        character (len=*), intent(in)::file_name
-        type(element), allocatable, intent(inout)::meshele(:)
-        type(face), allocatable, intent(inout)::meshface(:)
-        type(face), allocatable::bfaces(:) ! this is to store faces on boundaries
-            ! actual mesh faces will have interior faces as well.
-        type(vertex), allocatable, intent(inout)::meshvertex(:)
-        type(vertex), allocatable::vertices(:)  ! this only stores one copy 
-            ! of coordinates at same position. (but there can be multiple nodes)
-        integer :: ierr , i, nnod, idx, ele, ele2, nface, inod
-        integer :: edge_nodes(2)
-        integer :: nelements=0, npoints=0, nbcface=0, nele=0
-        real, dimension(3) :: coor
-        integer, dimension(8):: element_info
-        character(len=255) :: text, t, word
-        
-        open(unit=10, file=file_name, status='old', action='read') 
-        do  
-            ! this find string snippet is from:
-            ! https://stackoverflow.com/questions/29125581/fortran-find-string-in-txt-file
-            ! by Fortranner
-            read (10, "(a)", iostat=ierr) text
-            if (ierr /= 0) exit 
-            read (text,*) word 
-            if (word == '$Nodes') then ! start reading nodes information
-                read (10, *) nnod
-                ! print*, 'nnod=', nnod
-                allocate(vertices(nnod))
-                do 
-                    read (10, *, iostat=ierr) idx, coor(1), coor(2), coor(3)
-                    if (ierr /= 0) exit 
-                    vertices(idx)%coor(1) = coor(1)
-                    vertices(idx)%coor(2) = coor(2)
-                    ! print*, 'coor', coor
-                enddo
-            endif
-            if (word == '$Elements') then ! start reading element information 
-                read (10, *) nelements
-                ! print *, 'nelements=', nelements
-                do  
-                    read (10, "(A)", iostat=ierr) text
-                    ! print *, text 
-                    if (ierr /= 0) exit 
-                    if (text(1:12)=="$EndElements") exit
-                    i=0
-                    ! print *, text
-                    do while (text /= "")
-                        i=i+1
-                        call GetTag(t,text)
-                        read(t,*) element_info(i)
-                    enddo
-                    select case (i)
-                    case (6) ! 0D elements (points)
-                        npoints = npoints + 1 
-                    case (7) ! 1D elements (edges)
-                        nbcface = nbcface + 1
-                    case (8) ! 2D elements (faces)
-                        nele = nele + 1
-                    case default 
-                        print *, 'something is wrong when parsing Elements data, what is being parsed is:'
-                        print *, text
-                    end select 
-                    ! print*, 'npoints=',npoints, 'nbcface=', nbcface, 'nele=', nele
-                enddo 
-            endif
-        enddo
-        allocate(bfaces(nbcface), meshele(nele)) 
-        rewind (10) ! go back and read again to store node info associated with 
-                    ! points, edges, and faces.
-        do  
-            read (10, "(a)", iostat=ierr) text
-            if (ierr /= 0) exit 
-            read (text,*) word 
-            if (word == '$Elements') then ! start reading element information
-                ! jump 0D elements
-                do i=1,npoints+1
-                    read(10,*)
-                enddo
-                ! 1D elements
-                do nface=1,nbcface 
-                    read (10, "(A)", iostat=ierr) text 
-                    i=0
-                    do while (text /= "")
-                        i=i+1
-                        call GetTag(t,text)
-                        read(t,*) element_info(i)
-                    enddo
-                    bfaces(nface)%vertex(1) = element_info(6)
-                    bfaces(nface)%vertex(2) = element_info(7)
-                    bfaces(nface)%bctype = 1
-                    ! print *, 'bfaces=', bfaces(nface)%vertex
-                enddo
-                ! 2D elements
-                do ele=1,nele
-                    read (10, "(A)", iostat=ierr) text 
-                    i=0
-                    do while (text /= "")
-                        i=i+1
-                        call GetTag(t,text)
-                        read(t,*) element_info(i)
-                        ! print *, 'text=',text,'t=', t, 'element_info', element_info(i)
-                    enddo
-                    meshele(ele)%node(1) = element_info(6)
-                    meshele(ele)%node(2) = element_info(7)
-                    meshele(ele)%node(3) = element_info(8)
-                    ! print *, 'meshele=', meshele(ele)
-                enddo
-            endif
-        enddo
-        close(10) ! gmsh file no longer has useful info for us.
-
-        ! now we are going to build edges and ... vertices actually. (DG)
-        allocate(meshvertex(nele*3), meshface(nele*3)) ! every 2D element 
-            ! has 3 vertices and 3 faces
-        do ele = 1,nele 
-            ! brutely build meshvertex and mesh face
-            do inod = 1,3
-                ! global nodes
-                meshvertex( (ele-1)*3+inod )%coor(:) = &
-                    vertices( meshele(ele)%node(inod) )%coor(:)
-                    ! this gives us a natural local to global
-                    ! map. I guess IC-FERST uses the same way?
-                ! print*, 'coordinate for global node', (ele-1)*3+inod, 'is' ,meshvertex( (ele-1)*3+inod )%coor
-                
-                ! global edges' nodes 
-                    ! (local numbering)
-                    !  3
-                    !  | \
-                    !  |  \
-                    !  3   2
-                    !  |    \
-                    !  1--1--2
-                if (inod.eq.3) edge_nodes=(/3,1/)
-                if (inod.ne.3) edge_nodes=(/inod, inod+1/)
-                meshface( (ele-1)*3+inod )%vertex(1) = (ele-1)*3+edge_nodes(1)
-                meshface( (ele-1)*3+inod )%vertex(2) = (ele-1)*3+edge_nodes(2)
-                meshele( ele )%face(inod) = (ele-1)*3+inod
-                ! print*, 'global nodes for edge', (ele-1)*3+inod, 'is ', meshface( (ele-1)*3+inod )%vertex
-
-                ! is this meshface a boundary face or an interior face?
-                meshface( (ele-1)*3+inod )%bctype=0 ! default: interior face
-                do i = 1,nbcface
-                    ! print *, 'elenode1=', meshele(ele)%node(inod), &
-                    !     'elenode2=', meshele(ele)%node(edge_nodes(2)), &
-                    !     'bfaces nodes=', bfaces(i)%vertex
-                    if ( any( meshele(ele)%node(inod) .eq. bfaces(i)%vertex ) .and. &
-                        any( meshele(ele)%node(edge_nodes(2)) .eq. bfaces(i)%vertex)) then
-                        meshface( (ele-1)*3+inod )%bctype=1 ! is boundary, assuming Dirichlet
-                        exit
-                    endif
-                enddo
-                ! print *, 'edge type (bc or not)', meshface( (ele-1)*3+inod )%bctype
-
-                ! now we find and store neighbor elements to this meshface
-                meshface( (ele-1)*3+inod )%neighbor(1) = ele ! first, this face belongs to 
-                    ! its own element. Then we need to find its other neighbor
-                if ( meshface( (ele-1)*3+inod )%bctype.eq.0) then
-                    do ele2 = 1,nele
-                        ! print *, 'elenode1=', meshele(ele)%node(inod), &
-                        !     'elenode2=', meshele(ele)%node(edge_nodes(2)), &
-                        !     'ele2 nodes = ', meshele(ele2)%node
-                        if (any(meshele(ele)%node(inod) .eq. meshele(ele2)%node) .and. &
-                            any(meshele(ele)%node(edge_nodes(2)) .eq. meshele(ele2)%node) .and. & 
-                            ele .ne. ele2) then
-                            meshface( (ele-1)*3+inod )%neighbor(2) = ele2
-                            exit
-                        endif
-                    enddo
-                endif
-                ! print *, 'edge neighbor elements are', meshface( (ele-1)*3+inod )%neighbor
-
-            enddo
-        enddo
-        
-        contains
-        subroutine GetTag(t, s)     ! remove first token, t, from s
-            ! from http://computer-programming-forum.com/49-fortran/acb879484e8189d2.htm
-            ! author: Jos Bergervo
-            character(len=*), intent(in out) :: s
-            character(len=*), intent(out)    :: t
-            integer    :: ii
-            s = adjustl(s)          ! Now the 1st token is directly at the start
-            ii = index(s," ")
-            t = s(1:ii)              ! returning s(1:0) gives just empty string
-            s = s(ii:len(s))         ! Keep only the rest of string s
-        end subroutine GetTag
-    end subroutine gmsh_read
-      
+    contains
+    subroutine GetTag(t, s)   ! remove first token, t, from s
+      ! from http://computer-programming-forum.com/49-fortran/acb879484e8189d2.htm
+      ! author: Jos Bergervo
+      character(len=*), intent(in out) :: s
+      character(len=*), intent(out)  :: t
+      integer  :: ii
+      s = adjustl(s)      ! Now the 1st token is directly at the start
+      ii = index(s," ")
+      t = s(1:ii)        ! returning s(1:0) gives just empty string
+      s = s(ii:len(s))     ! Keep only the rest of string s
+    end subroutine GetTag
+  end subroutine gmsh_read
+    
 
 end module
--- main.F90
+++ main.F90
@@ -1,70 +1,17 @@
 program main
-    ! we don't need input file at this moment as this is not to serve as a universal 
-    ! solver. 
-    ! we will solve a problem with a known analytical solution.
+  ! we don't need input file at this moment as this is not to serve as a universal 
+  ! solver. 
+  ! we will solve a problem with a known analytical solution.
+  use mesh_type
+  use gmsh_input
+  implicit none
 
-    implicit none
+  ! variable declaration
+  type(element), allocatable::meshele(:)
+  type(face), allocatable::meshface(:)
+  type(vertex), allocatable::meshvertex(:)
+  character (len=255)::gmsh_filename='example/square.msh'
 
-    ! derived data types
-    ! vertices of elements (global nodes)
-    type :: vertex
-        real, dimension(2) :: coor
-    end type vertex
-    ! faces of elements (global edges)
-    type :: face
-        integer, dimension(2) :: vertex ! index of vertices
-        integer, dimension(2) :: neighbor ! global number of elements sharing the face
-        integer :: bctype ! boundary type, 0 for interior, 1 Dirichlet, 2 Neumann
-    end type face
-    ! elements
-    type :: element
-        integer, dimension(3) :: face 
-        integer, dimension(3) :: node
-    end type element 
-    
-    ! variable declaration
-    real :: Lx(2), Ly(2), hx, hy 
-    integer :: nnod, inod, jnod, itotal, jtotal, nedge, i
-    type(vertex), dimension(:), allocatable :: meshvertex
-    type(face), dimension(:), allocatable :: meshface
-    type(element), dimension(:), allocatable :: meshele
-    integer :: node_num, node_dim, element_num, element_order 
-    character (len=255) :: gmsh_filename='example/square.msh'
-    real, allocatable:: node_x(:,:)
-    integer, allocatable:: element_node(:,:)
-
-    ! ! domain and subdivision (triangulation)
-    ! Lx = (/0.,1./)    ! x range
-    ! Ly = (/0.,1./)    ! y range
-    ! hx = 0.1    ! x size
-    ! hy = 0.1    ! y size
-    ! itotal = int((Lx(2)-Lx(1)) / hx + 1)  ! x-direction nodes number
-    ! jtotal = int((Ly(2)-Ly(1)) / hy + 1)  ! y-direction nodes number
-    ! ! vertex coordinate
-    ! nnod = itotal * jtotal    ! total number of nodes
-    ! allocate(meshvertex(nnod))
-    ! do inod = 1,itotal 
-    !     do jnod = 1,jtotal 
-    !         meshvertex( (jnod-1)*jtotal + inod )%coor(1) = (inod-1)*hx + Lx(1)
-    !         meshvertex( (jnod-1)*jtotal + inod )%coor(2) = (jnod-1)*hy + Ly(1)
-    !     enddo
-    ! enddo
-    ! ! element faces (edges)
-    
-    call gmsh_size_read(gmsh_filename,node_num, node_dim, element_num, element_order)
-    
-    allocate ( node_x(1:node_dim,1:node_num) )
-    allocate ( element_node(1:element_order,1:element_num) )
-    call gmsh_data_read (gmsh_filename, node_dim, node_num, node_x, &
-    element_order, element_num, element_node)
-    
-    allocate ( meshvertex(node_num) )
-    allocate ( meshele(element_num) )
-    do i = 1, node_num
-        meshvertex%coor(i) = node_x(:,i)
-    enddo
-    do i = 1, element_num 
-        print *, element_node(:,i), i
-    enddo
-
+  ! read in mesh and create connectivity
+  call gmsh_read(gmsh_filename, meshele, meshface, meshvertex)
 end program
