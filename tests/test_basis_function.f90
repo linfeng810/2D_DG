2e30a53821e2fb1dfb9c14e02aa0072ecbb735de
linfeng810 <li.linfeng.xjtu@gmail.com>
2021-10-16 11:04:15 +0100

    including test reading a gmsh file

--- tests/test_basis_function.f90
+++ tests/test_basis_function.f90
@@ -3,6 +3,7 @@
 program main
     use mesh_type
     use basis_function
+    use gmsh_input
 
     implicit none 
 
@@ -10,6 +11,13 @@ program main
     type(shape_func_dev) :: loc_sf 
     type(element) :: test_ele 
     type(vertex), dimension(3):: vertices
+
+    type(element), allocatable::meshele(:)
+    type(face), allocatable::meshface(:)
+    type(vertex), allocatable::meshvertex(:)
+    character (len=255)::gmsh_filename='example/square.msh'
+
+    integer::ele, nele, iface
     
     test_ele%node(:) = (/1,2,3/)
     vertices(1)%coor(:) = (/1.5,2.4/)
@@ -20,6 +28,7 @@ program main
     call calc_local_shape_func(loc_sf, ref_sf, test_ele, vertices)
 
     open(unit=10, file='tests/output.out', status='replace')
+    write(10,*) ' ======================= trivial test =================='
     write(10,*) '  basis functions at reference element:'
     write(10,*) '  (values on Gaussian quadrature point)'
     write(10,*) '  N1,              N2,              N3'
@@ -34,6 +43,26 @@ program main
     write(10,*) '  determinant x weight'
     write(10,*) loc_sf%detwei
     
+    write(10,*) ' ======================= test on square.msh ==========='
+    call gmsh_read(gmsh_filename, meshele, meshface, meshvertex)
+    nele = size(meshele)
+    write(10, *) 'nele=', nele
+    write(10, '(2A12,999A26)') 'ele', 'iface', 'dev_funs111', 'dev_funs121', 'dev_funs131', &
+        'dev_funs211', 'dev_funs221', 'dev_funs231', 'detwei', &
+        'sdx111', 'sdy111', 'sdx211', 'sdy211', 'sdx311', 'sdy311',  &
+        'sdx121', 'sdy121', 'sdx221', 'sdy221', 'sdx321', 'sdy321', &   ! surface derivative x/y of nodal shape function 1/2/3 on face 1/2/3
+        '...', '...', '...', '...', '...', '...', &
+        'sdetwei'
+    do ele = 1,nele
+        print*, 'input ele', ele
+        call calc_local_shape_func(loc_sf, ref_sf, meshele(ele), meshvertex)
+        do iface = 1,3
+            ! print*, 'input outside face', meshface( meshele(ele)%face(iface) )
+            call calc_local_surf_sf(loc_sf, ref_sf, meshface( meshele(ele)%face(iface) ), meshvertex)
+            write(10,'(2I12,999E26.10)') ele, iface, loc_sf 
+        enddo
+    enddo
+    write(10, *) 'end'
     close(10)
     
 end program
