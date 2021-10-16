9206bd5769b84230b845c3db1e190b14a5229b58
linfeng810 <li.linfeng.xjtu@gmail.com>
2021-10-16 11:00:22 +0100

    tweaks: remap meshele%node to global node numbering (instead of singlet vertex); change readin mesh file unit to newunit ... to avoid conflict

--- gmsh_input.F90
+++ gmsh_input.F90
@@ -10,12 +10,12 @@ module gmsh_input
   contains 
   
   subroutine gmsh_read(file_name, meshele, meshface, meshvertex)
-    character (len=*), intent(in)::file_name
-    type(element), allocatable, intent(inout)::meshele(:)
-    type(face), allocatable, intent(inout)::meshface(:)
+    character (len=*), intent(in)::file_name  ! gmsh file name
+    type(element), allocatable, intent(inout)::meshele(:) ! to store elements
+    type(face), allocatable, intent(inout)::meshface(:) ! to store element faces, number = 3 * element number, including interior and boundary faces
     type(face), allocatable::bfaces(:) ! this is to store faces on boundaries
       ! actual mesh faces will have interior faces as well.
-    type(vertex), allocatable, intent(inout)::meshvertex(:)
+    type(vertex), allocatable, intent(inout)::meshvertex(:) ! to store element nodes, number = 3 * element number
     type(vertex), allocatable::vertices(:)  ! this only stores one copy 
       ! of coordinates at same position. (but there can be multiple nodes)
     integer :: ierr , i, nnod, idx, ele, ele2, nface, inod
@@ -24,37 +24,46 @@ module gmsh_input
     real, dimension(3) :: coor
     integer, dimension(8):: element_info
     character(len=255) :: text, t, word
+    integer :: mesh_file_unit
     
-    open(unit=10, file=file_name, status='old', action='read') 
+    open(newunit=mesh_file_unit, file=file_name, status='old', action='read') 
+
+    ! first read in gmsh file raw data: 
+    ! 1. nodes coordinate
+    ! 2. boundary edge and nodes associated with it 
+    !    (in the future we can use physical id to indentify different bc types)
+    ! 3. element and nodes associated with it
     do  
       ! this find string snippet is from:
       ! https://stackoverflow.com/questions/29125581/fortran-find-string-in-txt-file
       ! by Fortranner
-      read (10, "(a)", iostat=ierr) text
+      read (mesh_file_unit, "(a)", iostat=ierr) text
       if (ierr /= 0) exit 
       read (text,*) word 
       if (word == '$Nodes') then ! start reading nodes information
-        read (10, *) nnod
+        read (mesh_file_unit, *) nnod ! total number of nodes
         ! print*, 'nnod=', nnod
         allocate(vertices(nnod))
         do 
-          read (10, *, iostat=ierr) idx, coor(1), coor(2), coor(3)
-          if (ierr /= 0) exit 
+          read (mesh_file_unit, *, iostat=ierr) idx, coor(1), coor(2), coor(3)
+          if (ierr /= 0) exit ! if read in '$EndNodes', will exit this loop
           vertices(idx)%coor(1) = coor(1)
           vertices(idx)%coor(2) = coor(2)
-          ! print*, 'coor', coor
+          ! print*, 'idx', idx, 'coor', coor
         enddo
       endif
       if (word == '$Elements') then ! start reading element information 
-        read (10, *) nelements
+        read (mesh_file_unit, *) nelements  ! total number of elements (1D, 2D, 3D)
         ! print *, 'nelements=', nelements
         do  
-          read (10, "(A)", iostat=ierr) text
+          read (mesh_file_unit, "(A)", iostat=ierr) text
           ! print *, text 
           if (ierr /= 0) exit 
           if (text(1:12)=="$EndElements") exit
           i=0
           ! print *, text
+          ! parsing element information in one line
+          !   according to gmsh document, this line may contain various (6-8) numbers
           do while (text /= "")
             i=i+1
             call GetTag(t,text)
@@ -62,11 +71,11 @@ module gmsh_input
           enddo
           select case (i)
           case (6) ! 0D elements (points)
-            npoints = npoints + 1 
+            npoints = npoints + 1 ! useless
           case (7) ! 1D elements (edges)
-            nbcface = nbcface + 1
+            nbcface = nbcface + 1 ! number of boundary faces
           case (8) ! 2D elements (faces)
-            nele = nele + 1
+            nele = nele + 1 ! number of 2D elements (actual *DG* elements)
           case default 
             print *, 'something is wrong when parsing Elements data, what is being parsed is:'
             print *, text
@@ -75,21 +84,23 @@ module gmsh_input
         enddo 
       endif
     enddo
+
     allocate(bfaces(nbcface), meshele(nele)) 
-    rewind (10) ! go back and read again to store node info associated with 
+
+    rewind (mesh_file_unit) ! go back and read again to store node info associated with 
           ! points, edges, and faces.
     do  
-      read (10, "(a)", iostat=ierr) text
+      read (mesh_file_unit, "(a)", iostat=ierr) text
       if (ierr /= 0) exit 
       read (text,*) word 
       if (word == '$Elements') then ! start reading element information
         ! jump 0D elements
         do i=1,npoints+1
-          read(10,*)
+          read(mesh_file_unit,*)
         enddo
         ! 1D elements
         do nface=1,nbcface 
-          read (10, "(A)", iostat=ierr) text 
+          read (mesh_file_unit, "(A)", iostat=ierr) text 
           i=0
           do while (text /= "")
             i=i+1
@@ -103,7 +114,7 @@ module gmsh_input
         enddo
         ! 2D elements
         do ele=1,nele
-          read (10, "(A)", iostat=ierr) text 
+          read (mesh_file_unit, "(A)", iostat=ierr) text 
           i=0
           do while (text /= "")
             i=i+1
@@ -114,11 +125,11 @@ module gmsh_input
           meshele(ele)%node(1) = element_info(6)
           meshele(ele)%node(2) = element_info(7)
           meshele(ele)%node(3) = element_info(8)
-          ! print *, 'meshele=', meshele(ele)
+          ! print *, 'ele', ele, 'meshele=', meshele(ele)%node
         enddo
       endif
     enddo
-    close(10) ! gmsh file no longer has useful info for us.
+    close(mesh_file_unit) ! gmsh file no longer has useful info for us.
 
     ! now we are going to build edges and ... vertices actually. (DG)
     allocate(meshvertex(nele*3), meshface(nele*3)) ! every 2D element 
@@ -131,6 +142,8 @@ module gmsh_input
           vertices( meshele(ele)%node(inod) )%coor(:)
           ! this gives us a natural local to global
           ! map. I guess IC-FERST uses the same way?
+        ! meshele(ele)%node(inod) = (ele-1)*3+inod ! change element node numbering to DG global numbering
+          ! can't do it here because we need this information to find neighbor element!
         ! print*, 'coordinate for global node', (ele-1)*3+inod, 'is' ,meshvertex( (ele-1)*3+inod )%coor
         
         ! global edges' nodes 
@@ -180,6 +193,13 @@ module gmsh_input
         endif
         ! print *, 'edge neighbor elements are', meshface( (ele-1)*3+inod )%neighbor
 
+      enddo
+    enddo
+
+    ! after all, change element node numbering to DG global numbering
+    do ele=1,nele 
+      do inod = 1,3
+        meshele(ele)%node(inod) = (ele-1)*3+inod 
       enddo
     enddo
     
