0ce5a56cfff6f8da2aa98c4f0322f4e131bc05f0
Linfeng Li <ll2920@ese-stroma.ese.ic.ac.uk>
2021-10-22 18:49:36 +0100

    more readable output

--- tests/test_gmsh_input.f90
+++ tests/test_gmsh_input.f90
@@ -13,16 +13,20 @@ program main
 
   call gmsh_read(gmsh_filename, meshele, meshface, meshvertex)
   open(unit=20, file='tests/output.out', status='replace')
+  write(20,'(999A12)') 'ele', 'face1', 'face2', 'face3', &
+    'node1', 'node2', 'node3'
   do i = 1,size(meshele)
-    write(20,*) i, '--', meshele(i)
+    write(20,'(999I12)') i, meshele(i)
   enddo
-  write(1,*)
+  write(20,'(999A12)') 'face', 'node1', 'node2', &
+    'nb_ele_1', 'nb_ele_2', 'bctype' , 'nb_node_1', 'nb_node_2', &
+    'lc_node_1', 'lc_node_2', 'nb_iface'
   do i = 1,size(meshface)
-    write(20,*) i, '--',  meshface(i)
+    write(20,'(999I12)') i, meshface(i)
   enddo
-  write(1,*)
+  write(20,'(999A12)') 'vertex', 'x', 'y'
   do i = 1,size(meshvertex)
-    write(20,*) i, '--', meshvertex(i)
+    write(20,'(I12,2F12.5)') i, meshvertex(i)
   enddo
   close(20)
   print*, 'complete'
